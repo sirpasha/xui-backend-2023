0ea513d00acd1aeead3d60d61babae87edd863fb
Pasha Ghomi <54184472+sirpasha@users.noreply.github.com>
2023-02-02 20:25:51 +0330

    user update apis added

--- Routes/Requests.js
+++ Routes/Requests.js
@@ -1260,6 +1260,283 @@ router.get('/reseller', async (req, res, error) => {
     }
 });
 
+// Update User on US Server ##########################################################################################################################
+router.post('/us/revise', async (req, res, err) => {
+    const serverUrl = process.env.US;
+    await axios.post(`${serverUrl}:61501/login`, data, { httpsAgent: httpsAgent})
+    .then (async (response) => {
+        const receivedCookie = response.headers['set-cookie'];
+        const cookie = JSON.stringify(receivedCookie).replace(/[\])}[{(]/g, '');
+        const theCookie = cookie.replace('"', '');
+        const headers = {
+            headers: {
+                "Cookie": theCookie,
+                "Content-type": 'application/x-www-form-urlencoded'
+            }
+        };
+
+        
+        function unixTimestamp () {  
+            return Math.floor(Date.now() / 1000)
+        }
+
+        const id = req.body.id;
+        const remark = req.body.remark;
+        const port = req.body.port;
+        const theUUId = req.body.uuid;
+        const total = 32212254720;
+        const month = 30 * 24 * 3600;
+        const expTime = (unixTimestamp() + month) * 1000;
+        const raw = `up=0&down=0&total=${total}&remark=${remark}&enable=true&expiryTime=${expTime}&listen=&port=${port}&protocol=vmess&settings=%7B%0A%20%20%22clients%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22id%22%3A%20%22${theUUId}%22%2C%0A%20%20%20%20%20%20%22alterId%22%3A%200%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22disableInsecureEncryption%22%3A%20false%0A%7D&streamSettings=%7B%0A%20%20%22network%22%3A%20%22ws%22%2C%0A%20%20%22security%22%3A%20%22tls%22%2C%0A%20%20%22tlsSettings%22%3A%20%7B%0A%20%20%20%20%22serverName%22%3A%20%22us.aqaqomi.ir%22%2C%0A%20%20%20%20%22certificates%22%3A%20%5B%0A%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22certificateFile%22%3A%20%22%2Froot%2Fcert%2Fus.aqaqomi.ir.cer%22%2C%0A%20%20%20%20%20%20%20%20%22keyFile%22%3A%20%22%2Froot%2Fcert%2Fus.aqaqomi.ir.key%22%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%5D%2C%0A%20%20%20%20%22alpn%22%3A%20%5B%5D%0A%20%20%7D%2C%0A%20%20%22wsSettings%22%3A%20%7B%0A%20%20%20%20%22acceptProxyProtocol%22%3A%20false%2C%0A%20%20%20%20%22path%22%3A%20%22%2F%22%2C%0A%20%20%20%20%22headers%22%3A%20%7B%7D%0A%20%20%7D%0A%7D&sniffing=%7B%0A%20%20%22enabled%22%3A%20true%2C%0A%20%20%22destOverride%22%3A%20%5B%0A%20%20%20%20%22http%22%2C%0A%20%20%20%20%22tls%22%0A%20%20%5D%0A%7D`;
+        try {
+            await axios.post(`${serverUrl}:61501/xui/inbound/update/${id}`, raw, headers, { httpsAgent: httpsAgent })
+            .then(response => {
+                creditManage(req.headers.token);
+                res.status(200).json({
+                    msg: "User Updated Successfully"
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
+        } catch {
+            res.status(400).json({
+                err: "Error Updating User"
+            });
+        }
+        
+                
+        })
+        .catch(err => {
+            res.status(400).json({
+                err: "Invalid Login"
+            });
+        });
+
+
+    });
+
+// Update User on FR Server ##########################################################################################################################
+router.post('/fr/revise', async (req, res, err) => {
+    const serverUrl = process.env.FR;
+    await axios.post(`${serverUrl}:61501/login`, data, { httpsAgent: httpsAgent})
+    .then (async (response) => {
+        const receivedCookie = response.headers['set-cookie'];
+        const cookie = JSON.stringify(receivedCookie).replace(/[\])}[{(]/g, '');
+        const theCookie = cookie.replace('"', '');
+        const headers = {
+            headers: {
+                "Cookie": theCookie,
+                "Content-type": 'application/x-www-form-urlencoded'
+            }
+        };
+
+        
+        function unixTimestamp () {  
+            return Math.floor(Date.now() / 1000)
+        }
+
+        const id = req.body.id;
+        const remark = req.body.remark;
+        const port = req.body.port;
+        const theUUId = req.body.uuid;
+        const total = 53687091200;
+        const month = 30 * 24 * 3600;
+        const expTime = (unixTimestamp() + month) * 1000;
+        const raw = `up=0&down=0&total=${total}&remark=${remark}&enable=true&expiryTime=${expTime}&listen=&port=${port}&protocol=vmess&settings=%7B%0A%20%20%22clients%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22id%22%3A%20%22${theUUId}%22%2C%0A%20%20%20%20%20%20%22alterId%22%3A%200%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22disableInsecureEncryption%22%3A%20false%0A%7D&streamSettings=%7B%0A%20%20%22network%22%3A%20%22ws%22%2C%0A%20%20%22security%22%3A%20%22tls%22%2C%0A%20%20%22tlsSettings%22%3A%20%7B%0A%20%20%20%20%22serverName%22%3A%20%22fr.aqaqomi.ir%22%2C%0A%20%20%20%20%22certificates%22%3A%20%5B%0A%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22certificateFile%22%3A%20%22%2Froot%2Fcert%2Ffr.aqaqomi.ir.cer%22%2C%0A%20%20%20%20%20%20%20%20%22keyFile%22%3A%20%22%2Froot%2Fcert%2Ffr.aqaqomi.ir.key%22%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%5D%2C%0A%20%20%20%20%22alpn%22%3A%20%5B%5D%0A%20%20%7D%2C%0A%20%20%22wsSettings%22%3A%20%7B%0A%20%20%20%20%22acceptProxyProtocol%22%3A%20false%2C%0A%20%20%20%20%22path%22%3A%20%22%2F%22%2C%0A%20%20%20%20%22headers%22%3A%20%7B%7D%0A%20%20%7D%0A%7D&sniffing=%7B%0A%20%20%22enabled%22%3A%20true%2C%0A%20%20%22destOverride%22%3A%20%5B%0A%20%20%20%20%22http%22%2C%0A%20%20%20%20%22tls%22%0A%20%20%5D%0A%7D`;
+        try {
+            await axios.post(`${serverUrl}:61501/xui/inbound/update/${id}`, raw, headers, { httpsAgent: httpsAgent })
+            .then(response => {
+                creditManage(req.headers.token);
+                res.status(200).json({
+                    msg: "User Updated Successfully"
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
+        } catch {
+            res.status(400).json({
+                err: "Error Updating User"
+            });
+        }
+        
+                
+        })
+        .catch(err => {
+            res.status(400).json({
+                err: "Invalid Login"
+            });
+        });
+
+
+    });
+// Update User on DE Server ##########################################################################################################################
+router.post('/de/revise', async (req, res, err) => {
+    const serverUrl = process.env.DE;
+    await axios.post(`${serverUrl}:61501/login`, data, { httpsAgent: httpsAgent})
+    .then (async (response) => {
+        const receivedCookie = response.headers['set-cookie'];
+        const cookie = JSON.stringify(receivedCookie).replace(/[\])}[{(]/g, '');
+        const theCookie = cookie.replace('"', '');
+        const headers = {
+            headers: {
+                "Cookie": theCookie,
+                "Content-type": 'application/x-www-form-urlencoded'
+            }
+        };
+
+        
+        function unixTimestamp () {  
+            return Math.floor(Date.now() / 1000)
+        }
+
+        const id = req.body.id;
+        const remark = req.body.remark;
+        const port = req.body.port;
+        const theUUId = req.body.uuid;
+        const total = 53687091200;
+        const month = 30 * 24 * 3600;
+        const expTime = (unixTimestamp() + month) * 1000;
+        const raw = `up=0&down=0&total=${total}&remark=${remark}&enable=true&expiryTime=${expTime}&listen=&port=${port}&protocol=vmess&settings=%7B%0A%20%20%22clients%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22id%22%3A%20%22${theUUId}%22%2C%0A%20%20%20%20%20%20%22alterId%22%3A%200%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22disableInsecureEncryption%22%3A%20false%0A%7D&streamSettings=%7B%0A%20%20%22network%22%3A%20%22ws%22%2C%0A%20%20%22security%22%3A%20%22tls%22%2C%0A%20%20%22tlsSettings%22%3A%20%7B%0A%20%20%20%20%22serverName%22%3A%20%22de.aqaqomi.ir%22%2C%0A%20%20%20%20%22certificates%22%3A%20%5B%0A%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22certificateFile%22%3A%20%22%2Froot%2Fcert%2Fde.aqaqomi.ir.cer%22%2C%0A%20%20%20%20%20%20%20%20%22keyFile%22%3A%20%22%2Froot%2Fcert%2Fde.aqaqomi.ir.key%22%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%5D%2C%0A%20%20%20%20%22alpn%22%3A%20%5B%5D%0A%20%20%7D%2C%0A%20%20%22wsSettings%22%3A%20%7B%0A%20%20%20%20%22acceptProxyProtocol%22%3A%20false%2C%0A%20%20%20%20%22path%22%3A%20%22%2F%22%2C%0A%20%20%20%20%22headers%22%3A%20%7B%7D%0A%20%20%7D%0A%7D&sniffing=%7B%0A%20%20%22enabled%22%3A%20true%2C%0A%20%20%22destOverride%22%3A%20%5B%0A%20%20%20%20%22http%22%2C%0A%20%20%20%20%22tls%22%0A%20%20%5D%0A%7D`;
+        try {
+            await axios.post(`${serverUrl}:61501/xui/inbound/update/${id}`, raw, headers, { httpsAgent: httpsAgent })
+            .then(response => {
+                creditManage(req.headers.token);
+                res.status(200).json({
+                    msg: "User Updated Successfully"
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
+        } catch {
+            res.status(400).json({
+                err: "Error Updating User"
+            });
+        }
+        
+                
+        })
+        .catch(err => {
+            res.status(400).json({
+                err: "Invalid Login"
+            });
+        });
+
+
+    });
+// Update User on NL Server ##########################################################################################################################
+router.post('/nl/revise', async (req, res, err) => {
+    const serverUrl = process.env.NL;
+    await axios.post(`${serverUrl}:61501/login`, data, { httpsAgent: httpsAgent})
+    .then (async (response) => {
+        const receivedCookie = response.headers['set-cookie'];
+        const cookie = JSON.stringify(receivedCookie).replace(/[\])}[{(]/g, '');
+        const theCookie = cookie.replace('"', '');
+        const headers = {
+            headers: {
+                "Cookie": theCookie,
+                "Content-type": 'application/x-www-form-urlencoded'
+            }
+        };
+
+        
+        function unixTimestamp () {  
+            return Math.floor(Date.now() / 1000)
+        }
+
+        const id = req.body.id;
+        const remark = req.body.remark;
+        const port = req.body.port;
+        const theUUId = req.body.uuid;
+        const total = 32212254720;
+        const month = 30 * 24 * 3600;
+        const expTime = (unixTimestamp() + month) * 1000;
+        const raw = `up=0&down=0&total=${total}&remark=${remark}&enable=true&expiryTime=${expTime}&listen=&port=${port}&protocol=vmess&settings=%7B%0A%20%20%22clients%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22id%22%3A%20%22${theUUId}%22%2C%0A%20%20%20%20%20%20%22alterId%22%3A%200%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22disableInsecureEncryption%22%3A%20false%0A%7D&streamSettings=%7B%0A%20%20%22network%22%3A%20%22ws%22%2C%0A%20%20%22security%22%3A%20%22tls%22%2C%0A%20%20%22tlsSettings%22%3A%20%7B%0A%20%20%20%20%22serverName%22%3A%20%22nl.aqaqomi.ir%22%2C%0A%20%20%20%20%22certificates%22%3A%20%5B%0A%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22certificateFile%22%3A%20%22%2Froot%2Fcert%2Fnl.aqaqomi.ir.cer%22%2C%0A%20%20%20%20%20%20%20%20%22keyFile%22%3A%20%22%2Froot%2Fcert%2Fnl.aqaqomi.ir.key%22%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%5D%2C%0A%20%20%20%20%22alpn%22%3A%20%5B%5D%0A%20%20%7D%2C%0A%20%20%22wsSettings%22%3A%20%7B%0A%20%20%20%20%22acceptProxyProtocol%22%3A%20false%2C%0A%20%20%20%20%22path%22%3A%20%22%2F%22%2C%0A%20%20%20%20%22headers%22%3A%20%7B%7D%0A%20%20%7D%0A%7D&sniffing=%7B%0A%20%20%22enabled%22%3A%20true%2C%0A%20%20%22destOverride%22%3A%20%5B%0A%20%20%20%20%22http%22%2C%0A%20%20%20%20%22tls%22%0A%20%20%5D%0A%7D`;
+        try {
+            await axios.post(`${serverUrl}:61501/xui/inbound/update/${id}`, raw, headers, { httpsAgent: httpsAgent })
+            .then(response => {
+                creditManage(req.headers.token);
+                res.status(200).json({
+                    msg: "User Updated Successfully"
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
+        } catch {
+            res.status(400).json({
+                err: "Error Updating User"
+            });
+        }
+        
+                
+        })
+        .catch(err => {
+            res.status(400).json({
+                err: "Invalid Login"
+            });
+        });
+
+
+    });
+// Update User on FI Server ##########################################################################################################################
+router.post('/fi/revise', async (req, res, err) => {
+    const serverUrl = process.env.FI;
+    await axios.post(`${serverUrl}:61501/login`, data, { httpsAgent: httpsAgent})
+    .then (async (response) => {
+        const receivedCookie = response.headers['set-cookie'];
+        const cookie = JSON.stringify(receivedCookie).replace(/[\])}[{(]/g, '');
+        const theCookie = cookie.replace('"', '');
+        const headers = {
+            headers: {
+                "Cookie": theCookie,
+                "Content-type": 'application/x-www-form-urlencoded'
+            }
+        };
+
+        
+        function unixTimestamp () {  
+            return Math.floor(Date.now() / 1000)
+        }
+
+        const id = req.body.id;
+        const remark = req.body.remark;
+        const port = req.body.port;
+        const theUUId = req.body.uuid;
+        const total = 53687091200;
+        const month = 30 * 24 * 3600;
+        const expTime = (unixTimestamp() + month) * 1000;
+        const raw = `up=0&down=0&total=${total}&remark=${remark}&enable=true&expiryTime=${expTime}&listen=&port=${port}&protocol=vmess&settings=%7B%0A%20%20%22clients%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22id%22%3A%20%22${theUUId}%22%2C%0A%20%20%20%20%20%20%22alterId%22%3A%200%0A%20%20%20%20%7D%0A%20%20%5D%2C%0A%20%20%22disableInsecureEncryption%22%3A%20false%0A%7D&streamSettings=%7B%0A%20%20%22network%22%3A%20%22ws%22%2C%0A%20%20%22security%22%3A%20%22tls%22%2C%0A%20%20%22tlsSettings%22%3A%20%7B%0A%20%20%20%20%22serverName%22%3A%20%22fi.aqaqomi.ir%22%2C%0A%20%20%20%20%22certificates%22%3A%20%5B%0A%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22certificateFile%22%3A%20%22%2Froot%2Fcert%2Ffi.aqaqomi.ir.cer%22%2C%0A%20%20%20%20%20%20%20%20%22keyFile%22%3A%20%22%2Froot%2Fcert%2Ffi.aqaqomi.ir.key%22%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%5D%2C%0A%20%20%20%20%22alpn%22%3A%20%5B%5D%0A%20%20%7D%2C%0A%20%20%22wsSettings%22%3A%20%7B%0A%20%20%20%20%22acceptProxyProtocol%22%3A%20false%2C%0A%20%20%20%20%22path%22%3A%20%22%2F%22%2C%0A%20%20%20%20%22headers%22%3A%20%7B%7D%0A%20%20%7D%0A%7D&sniffing=%7B%0A%20%20%22enabled%22%3A%20true%2C%0A%20%20%22destOverride%22%3A%20%5B%0A%20%20%20%20%22http%22%2C%0A%20%20%20%20%22tls%22%0A%20%20%5D%0A%7D`;
+        try {
+            await axios.post(`${serverUrl}:61501/xui/inbound/update/${id}`, raw, headers, { httpsAgent: httpsAgent })
+            .then(response => {
+                creditManage(req.headers.token);
+                res.status(200).json({
+                    msg: "User Updated Successfully"
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
+        } catch {
+            res.status(400).json({
+                err: "Error Updating User"
+            });
+        }
+        
+                
+        })
+        .catch(err => {
+            res.status(400).json({
+                err: "Invalid Login"
+            });
+        });
+
+
+    });
+
 const creditManage = async (value) => {
     const token = jwt.verify(value, process.env.TOKEN, async (err,result) => {
         const user = await Resellers.findOneAndUpdate(
